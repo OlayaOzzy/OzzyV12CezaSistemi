f0a7d7f8b819f9052d4543bd88bdaa4447b4376d
Ozzy <57108047+TheOzi@users.noreply.github.com>
2020-10-27 12:17:23 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,19 @@
+const Discord = require("discord.js");
+const ayarlar = require('../ayarlar.json');
+let db = require("quick.db")
+module.exports = async(member) => {
+  let muterolu = ayarlar.muterolu || "muterolid"
+  let jailrolu = ayarlar.jailrolu || "jailroluid"
+  let boostrolu = ayarlar.boosterrolu || "boosterroluid"
 
+ let x = db.fetch(`Hapiste_${member.guild.name}_${member.id}`)
+ let y = db.fetch(`Mutede_${member.guild.name}_${member.id}`)
+  if(x) {
+  setTimeout(() => {
+member.roles.cache.has(boostrolu) ? member.roles.set([boostrolu,jailrolu]) : member.roles.set([jailrolu])
+}, 5000)
+}
+if(y) {
+member.roles.add(muteroluid).catch(err => console.log(err))
+}
+};
