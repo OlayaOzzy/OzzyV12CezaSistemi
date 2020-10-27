a359505a6e5cc96fb6443da5be136346086a231a
Ozzy <57108047+TheOzi@users.noreply.github.com>
2020-10-27 15:12:37 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,14 +6,14 @@ module.exports = async(member) => {
   let jailrolu = ayarlar.jailrolu || "jailroluid"
   let boostrolu = ayarlar.boosterrolu || "boosterroluid"
 
- let x = db.fetch(`Hapiste_${member.guild.name}_${member.id}`)
- let y = db.fetch(`Mutede_${member.guild.name}_${member.id}`)
+ let x = await  db.fetch(`Hapiste_${member.guild.name}_${member.id}`)
+ let y = await  db.fetch(`Mutede_${member.guild.name}_${member.id}`)
   if(x) {
   setTimeout(() => {
 member.roles.cache.has(boostrolu) ? member.roles.set([boostrolu,jailrolu]) : member.roles.set([jailrolu])
-}, 5000)
+}, 5000);
 }
 if(y) {
-member.roles.add(muteroluid).catch(err => console.log(err))
+member.roles.add(muteroluid).catch(err => console.log(err));
 }
 };
